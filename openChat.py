3283f290643ae4b583a1f32050c8908b41cd07a2
cfa532 <cfa532@yahoo.com>
2024-02-23 23:54:16 +0800

    Update openChat.py

--- openChat.py
+++ openChat.py
@@ -7,9 +7,23 @@ from langchain.chains import ConversationChain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
 from langchain_core.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-# from config import EMBEDDING_FUNC, LegalCase, llm_chain, LAW_COLLECTION_NAME, MAX_TOKENS, LLM
 from dotenv import load_dotenv
+
 load_dotenv()
+MAX_TOKEN = 4096
+
+from typing import Any, Dict, List, Union
+from langchain_core.messages import get_buffer_string
+from langchain_core.messages.ai import AIMessage
+from langchain_core.messages.human import HumanMessage
+from langchain.memory.chat_memory import BaseChatMemory
+
+def trim(messages, max_len):
+    if sum([len(m.content) for m in messages]) > max_len:
+        messages.pop(0)
+        trim(messages, max_len)
+    else:
+        return
 
 async def handler(websocket):
     class MyStreamingHandler(StreamingStdOutCallbackHandler):
@@ -21,9 +35,26 @@ async def handler(websocket):
             await websocket.send(json.dumps({"type": "stream", "data": token}))
             sys.stdout.flush()
 
+    class MyConversationWindowMemory(ConversationBufferWindowMemory):
+        def __init__(self) -> None:
+            super().__init__()
+
+        @property
+        def buffer_as_str(self) -> str:
+            """Exposes the buffer as a string in case return_messages is True."""
+            messages = self.chat_memory.messages[-self.k * 2 :] if self.k > 0 else []
+            """remove the early messages to keep buffer from exceeding max token length"""
+            trim(messages, MAX_TOKEN/2)
+
+            return get_buffer_string(
+                messages,
+                human_prefix=self.human_prefix,
+                ai_prefix=self.ai_prefix,
+            )
+    
     CHAT_LLM = ChatOpenAI(temperature=0, model="gpt-4", max_tokens=2048, streaming=True,
                         callbacks=[MyStreamingHandler()])     # ChatOpenAI cannot have max_token=-1
-    chain = ConversationChain(llm=CHAT_LLM, memory=ConversationBufferWindowMemory(k=6), verbose=True)
+    chain = ConversationChain(llm=CHAT_LLM, memory=ConversationBufferWindowMemory(), verbose=True)
     chain.output_parser=StrOutputParser()
 
     while True:
