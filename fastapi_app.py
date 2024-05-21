556b224cd60453289040b71ac953820a8af18d24
cfa532 <cfa532@yahoo.com>
2024-05-21 21:58:58 +0800

    bookkeeping

--- fastapi_app.py
+++ fastapi_app.py
@@ -13,7 +13,7 @@ from openaiCBHandler import get_cost_tracker_callback
 from dotenv import load_dotenv
 load_dotenv()
 
-from leither_api import get_user, register_in_db, delete_user, update_user, get_users, get_user_session
+from leither_api import get_user, register_in_db, delete_user, update_user, get_users, get_user_session, bookkeeping
 from utilities import ConnectionManager, MAX_TOKEN, UserIn, UserOut, UserInDB
 import time
 
@@ -224,6 +224,7 @@ async def websocket_endpoint(websocket: WebSocket):
                     "answer": resp, 
                     "tokens": cb.total_tokens,
                     "cost": cb.total_cost}))
+                bookkeeping(params["model"], cb.total_tokens, cb.total_cost, event["user"])
 
     except WebSocketDisconnect:
         connectionManager.disconnect(websocket)
--- leither_api.py
+++ leither_api.py
@@ -18,33 +18,7 @@ USER_NODE_ID = "pM6YSo4Edczo5VYM05hjsGxFtJF"
 mid = client.MMCreate(api.sid, api.uid, "", "ajchat app db", 2, 0x07276705)
 print("mid  ", mid)
 
-# so = subprocess.check_output("../darwin/Leither dht findpeer "+USER_NODE_ID, shell=True).decode('utf-8')
-# print(so)
-def get_user_session():
-    # given a node id, find valid IPs
-    ips = list(filter(lambda x: len(x)>6, client.GetVar(api.sid, "ips", USER_NODE_ID).split(",")))
-
-    public_ips = [ip for ip in ips if not is_local_network_ip(ip)]      # remove local network IP
-    ip = public_ips[0]
-
-    v4_ips = [ip for ip in public_ips if not is_ipv6(ip)]      # get ipv4 list
-    if len(v4_ips) > 0:
-        ip = v4_ips[0]      # v4 IP takes priority
-
-    user_client, session_id = get_user_client(ip)
-    return {"node_ip": ip, "sid": session_id}
-
-def get_user_client(user_node_ip):
-    user_client = hprose.HttpClient("http://"+ user_node_ip +"/webapi/")
-    result = user_client.Login("aj", "123456", "byname")
-    ppt = user_client.SignPPT(result.sid, {
-        "CertFor": "Self",
-        "Userid": result.uid,
-        "RequestService": "mimei"
-    }, 1)
-    user_client.RequestService(ppt)
-    return user_client, result.sid
-
+# registrate a real user account.
 def register_in_db(user: UserInDB):
     print(user)
     if not get_user(user.username):
@@ -75,10 +49,6 @@ def get_user(username, identifier):
         user = client.Hget(mmsid, USER_ACCOUNT_KEY, identifier)
         return UserInDB(**json.loads(user))
 
-def get_users():
-    mmsid = client.MMOpen(api.sid, mid, "last")
-    return [UserInDB(**json.loads(user.value)) for user in client.Hgetall(mmsid, USER_ACCOUNT_KEY)]
-
 def update_user(user: UserInDB):
     mmsid = client.MMOpen(api.sid, mid, "last")
     user_in_db = UserInDB(**json.loads(client.Hget(mmsid, USER_ACCOUNT_KEY, user.username)))
@@ -91,6 +61,17 @@ def update_user(user: UserInDB):
 def delete_user(username: str):
     mmsid_cur = client.MMOpen(api.sid, mid, "cur")
     client.Hdel(mmsid_cur, USER_ACCOUNT_KEY, username)
+    client.MMBackup(api.sid, mid, "", "delRef=true")
+
+
+def bookkeeping(llm, total_cost, total_tokens, username):
+    mmsid = client.MMOpen(api.sid, mid, "last")
+    user_in_db = UserInDB(**json.loads(client.Hget(mmsid, USER_ACCOUNT_KEY, username)))
+    user_in_db.token_usage[llm] += float(total_cost)
+    user_in_db.token_count[llm] = max(user_in_db.token_count[llm]-int(total_tokens), 0)
+
+    mmsid_cur = client.MMOpen(api.sid, mid, "cur")
+    client.Hset(mmsid_cur, USER_ACCOUNT_KEY, username, json.dumps(user_in_db.model_dump()))
     client.MMBackup(api.sid, mid, "", "delRef=true")
 
 # def get_user(username, password, identifier):
