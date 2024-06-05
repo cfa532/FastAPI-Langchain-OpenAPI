7e0721123f2f8e756e7e9c34c0777ebb06427c27
cfa532 <cfa532@yahoo.com>
2024-06-05 21:31:22 +0800

    Update openaiCBHandler.py

--- openaiCBHandler.py
+++ openaiCBHandler.py
@@ -95,9 +95,9 @@ class CostTrackerCallback(OpenAICallbackHandler):
 
     def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
         """Run when chain ends running."""
-        text_response = response.generations[0][0].text
-        encoding = tiktoken.get_encoding("cl100k_base")
-        self.completion_tokens = len(encoding.encode(text_response))
+        # text_response = response.generations[0][0].text
+        # encoding = tiktoken.get_encoding("cl100k_base")
+        # self.completion_tokens = len(encoding.encode(text_response))
         model_name = standardize_model_name(self.model_name)
         if model_name in MODEL_COST_PER_1K_TOKENS:
             completion_cost = get_openai_token_cost_for_model(
