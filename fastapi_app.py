d9144b88af6015f809e95f28a511964cb0735f34
cfa532 <cfa532@yahoo.com>
2024-06-11 18:16:53 +0800

    Update fastapi_app.py

--- fastapi_app.py
+++ fastapi_app.py
@@ -360,7 +360,7 @@ async def websocket_endpoint(websocket: WebSocket, token: str = Query()):
     except JWTError:
         print("JWTError")
         sys.stdout.flush()
-        websocket.send_text(json.dumps({"type": "error", "error": "Invalid token"}))
+        await websocket.send_text(json.dumps({"type": "error", "error": "Invalid token"}))
     except HTTPException as e:
         print("HTTPException", e)
         sys.stdout.flush()
