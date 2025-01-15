a25ec290e3963c1c8d41ff66f01e91a0ef854066
cfa532 <cfa532@yahoo.com>
2025-01-15 16:11:37 +0800

    Update geniChat.py

--- geniChat.py
+++ geniChat.py
@@ -1,7 +1,9 @@
-import json, sys, os, time, logging
-import google.generativeai as genai
-from langchain_google_genai import ChatGoogleGenerativeAI
-# from langchain_google_vertexai import ChatVertexAI
+import json, sys, os, time, io, base64, requests
+# import google.generativeai as genai
+from google.cloud import storage
+import vertexai
+from vertexai.generative_models import GenerativeModel, Part, ChatSession
+vertexai.init(project=os.environ.get("GOOGLE_CLOUD_PROJECT"), location="asia-east1")
 
 MAX_TOKEN = {
     "gemini-2.0-flash-exp": 8192,
@@ -10,62 +12,56 @@ MAX_TOKEN = {
 }
 
 async def geniChat(websocket, msg, lapi, user):
-    # os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "leither-ai-credentials.json"
     params = msg["parameters"]
-    userQuery = msg["input"]["query"]
-    model = genai.GenerativeModel(params["model"])
-    inputTokenCount = model.count_tokens(userQuery).total_tokens
+    instructions = msg["input"]["query"]
+    model = GenerativeModel(params["model"])
+    inputTokenCount = model.count_tokens(instructions).total_tokens
 
-    CHAT_LLM = ChatGoogleGenerativeAI(
-        model=params["model"],
-        temperature=0,
-        max_tokens=None,
-        timeout=None,
-        max_retries=2,
-        streaming=True,
-        verbose=True
-    )
-    query = "Human: " + userQuery + "\nAI:"
-    if msg["input"].get("history"):
-        # memory.clear()  # do not use memory on serverside.
-        # Add chat history kept by client.
-        for c in msg["input"]["history"]:
-            inputTokenCount += model.count_tokens(c["Q"] + c["A"]).total_tokens
-            if inputTokenCount > MAX_TOKEN[params["model"]]*2/3:
-                break
-            else:
-                query = "Human: "+c["Q"]+"\nAI: "+c["A"]+"\n" + query
-    start_time = time.time()
-    chain = CHAT_LLM
-    resp = ""
-    token_count = 0
-    cost = 0
-    async for chunk in chain.astream(query):
-        print(chunk.content, end="|", flush=True)    # chunk size can be big
-        resp += chunk.content
+    if len(msg["input"]["urls"]) > 0:
+        for url in msg["input"]["urls"]:
+            video_data = Part.from_uri(uri=url, mime_type="video/mp4")
+            response = model.generate_content([video_data, instructions])
+            print(response.usage_metadata)
+            await websocket.send_text(json.dumps({
+                "tokens": response.usage_metadata.total_token_count,
+                "cost": 0,
+                "type": "result",
+                "answer": response.text}))
+    else:
+        query = "Human: " + instructions + "\nAI:"
+        if msg["input"].get("history"):
+            # Add chat history kept by client.
+            for c in msg["input"]["history"]:
+                inputTokenCount += model.count_tokens(c["Q"] + c["A"]).total_tokens
+                if inputTokenCount > MAX_TOKEN[params["model"]]*2/3:
+                    break
+                else:
+                    query = "Human: "+c["Q"]+"\nAI: "+c["A"]+"\n" + query
 
-        # the last chuck contains the usage metadata
-        if chunk.usage_metadata is not None:
-            token_count = chunk.usage_metadata["total_tokens"]
-        await websocket.send_text(json.dumps({"type": "stream", "data": chunk.content}))
-    print("time diff=", (time.time() - start_time))
+        token_count = 0
+        cost = 0
+        answer = ""
+        start_time = time.time()
+        # chat_session = model.start_chat()
+        responses = model.generate_content(query, stream=True)
+
+        for chunk in responses:
+            answer += chunk.text
+            print(chunk)
+            # the last chuck contains the usage metadata
+            if chunk.usage_metadata and chunk.usage_metadata.total_token_count:
+                token_count = chunk.usage_metadata.total_token_count
+                print(chunk.usage_metadata)
+            await websocket.send_text(json.dumps({"type": "stream", "data": chunk.text}))
+
+        print("time diff=", (time.time() - start_time))
+        
+        await websocket.send_text(json.dumps({
+            "type": "result",
+            "answer": answer, 
+            "tokens": token_count,
+            "cost": cost}))
+
+        lapi.bookkeeping(params["model"], cost, token_count, user)
+
     sys.stdout.flush()
-    
-    await websocket.send_text(json.dumps({
-        "type": "result",
-        "answer": resp, 
-        "tokens": token_count,
-        "cost": cost}))
-
-    lapi.bookkeeping(params["model"], cost, token_count, user)
-
-###
-# content='Hello! 👋  How can I help you today? 😊 \n' additional_kwargs={} 
-# response_metadata={'is_blocked': False, 'safety_ratings': [{'category': 'HARM_CATEGORY_SEXUALLY_EXPLICIT', 
-# 'probability_label': 'NEGLIGIBLE', 'probability_score': 0.1533203125, 'blocked': False, 
-# 'severity': 'HARM_SEVERITY_NEGLIGIBLE', 'severity_score': 0.0244140625}], 
-# 'usage_metadata': {'prompt_token_count': 2, 'candidates_token_count': 14, 'total_token_count': 16,
-# 'cached_content_token_count': 0}, 'finish_reason': 'STOP', 'avg_logprobs': -0.046805624450956075}
-# id='run-64e16461-ec28-4133-97f6-17b4f839578d-0' 
-# usage_metadata={'input_tokens': 2, 'output_tokens': 14, 'total_tokens': 16}
-###
